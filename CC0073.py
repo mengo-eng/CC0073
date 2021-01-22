d5857fc389c7667ef100fd52ff5000002aff4b5a
mengo-eng <68229960+mengo-eng@users.noreply.github.com>
2021-01-22 00:32:44 +0000

    Update CC0073.py
    some changes made

--- CC0073.py
+++ CC0073.py
@@ -1,14 +1,12 @@
 result = 0
 n = input("Enter a number to check whether it is an Armstrong number: ")
 while n.isdigit() != True or int(n) == 0:
-    n = input("That wasn't an integer. Please, enter positive integer. ")
-    if n.isdigit() == True and int(n) != 0:
-        for i in n:
-            result += int(i) ** len(n)
-        if int(n) == result:
-            print(f"Congratulation! {n} is an Armstrong number!")
-        else:
-            print(f"Sorry! {n} is not an Armstrong number!")
-        break
-else:
-    print(n)
+    n = input("That wasn't an integer. Please, enter positive integer: ")
+
+if n.isdigit() == True and int(n) != 0:
+    for i in n:
+        result += int(i) ** len(n)
+    if int(n) == result:
+        print(f"Well Done! {n} is an Armstrong number!")
+    else:
+        print(f"Sorry! {n} is not an Armstrong number!")
